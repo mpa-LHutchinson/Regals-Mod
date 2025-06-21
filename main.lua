46fb6db457228ef1a76ab8a499ed92559fd89128
Liam Hutchinson <liamhutch1000@gmail.com>
2025-06-21 19:01:27 -0400

    added code for college joker and valentines day gift

--- main.lua
+++ main.lua
@@ -15,36 +15,34 @@ SMODS.Atlas{
     py = 95 -- height of one card
 }
 SMODS.Joker{
-    key = 'reginald', --joker key
+    key = 'collegejoker', --joker key
     loc_txt = { -- local text
-        name = 'Reginald',
+        name = 'College Joker',
         text = {
-          'When Blind is selected,',
-          'create a {C:attention}Joker{}',
-          '{X:mult,C:white}X#1#{} Mult',
-          'Gain {C:money}123${} at end of round'
+          '{X:mult,C:white}X#1#{} Mult if',
+          'hand is played',
+          'while in debt'
         },
         --[[unlock = {
             'Be {C:legendary}cool{}',
         }]]
     },
     atlas = 'Jokers', --atlas' key
-    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    rarity = 2, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
     soul_pos = { x = 0, y = 0 },
-    cost = 123, --cost
+    cost = 7, --cost
     unlocked = true, --where it is unlocked or not: if true, 
     discovered = true, --whether or not it starts discovered
     blueprint_compat = true, --can it be blueprinted/brainstormed/other
-    eternal_compat = false, --can it be eternal
-    perishable_compat = false, --can it be perishable
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
     pos = {x = 0, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
     config = { 
       extra = {
-        Xmult = 100 --configurable value
+        Xmult = 3 --configurable value
       }
     },
     loc_vars = function(self,info_queue,center)
-        info_queue[#info_queue+1] = G.P_CENTERS.j_joker --adds "Joker"'s description next to this card's description
         return {vars = {center.ability.extra.Xmult}} --#1# is replaced with card.ability.extra.Xmult
     end,
     check_for_unlock = function(self, args)
@@ -54,7 +52,7 @@ SMODS.Joker{
         unlock_card(self) --unlocks the card if it isnt unlocked
     end,
     calculate = function(self,card,context)
-        if context.joker_main then
+        if context.joker_main and G.GAME.dollars < 0 then
             return {
                 card = card,
                 Xmult_mod = card.ability.extra.Xmult,
@@ -62,22 +60,64 @@ SMODS.Joker{
                 colour = G.C.MULT
             }
         end
-
-        if context.setting_blind then
-            local new_card = create_card('Joker', G.jokers, nil,nil,nil,nil,'j_joker')
-            new_card:add_to_deck()
-            G.jokers:emplace(new_card)
+    end,
+    in_pool = function(self,wawa,wawa2)
+        --whether or not this card is in the pool, return true if it is, return false if its not
+        return true
+    end,
+}
+SMODS.Joker{
+    key = 'valentinesdaygift', --joker key
+    loc_txt = { -- local text
+        name = 'Valentines Day Gift',
+        text = {
+          'Each {C:hearts}Heart{} card',
+          'held in hand',
+          'scores {C:mult}+#1#{} mult'
+        },
+        --[[unlock = {
+            'Be {C:legendary}cool{}',
+        }]]
+    },
+    atlas = 'Jokers', --atlas' key
+    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    soul_pos = { x = 0, y = 0 },
+    cost = 5, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 0, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    config = { 
+      extra = {
+        mult = 5 --configurable value
+      }
+    },
+    loc_vars = function(self,info_queue,center)
+        return {vars = {center.ability.extra.mult}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+    check_for_unlock = function(self, args)
+        if args.type == 'derek_loves_you' then --not a real type, just a joke
+            unlock_card(self)
+        end
+        unlock_card(self) --unlocks the card if it isnt unlocked
+    end,
+    calculate = function(self,card,context)
+        if context.cardarea == G.hand and context.individual and not context.end_of_round and context.other_card:is_suit("Hearts") then
+            return {
+                card = card,
+                mult_mod = card.ability.extra.mult,
+                message = '+' .. card.ability.extra.mult,
+                colour = G.C.MULT
+            }
         end
     end,
     in_pool = function(self,wawa,wawa2)
         --whether or not this card is in the pool, return true if it is, return false if its not
         return true
     end,
-    calc_dollar_bonus = function(self,card)
-        return 123
-    end,
 }
-
 
   
 ----------------------------------------------
