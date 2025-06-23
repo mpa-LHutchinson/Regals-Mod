8b6201576ecad2d94f308335e097bd1118c80799
Liam Hutchinson <liamhutch1000@gmail.com>
2025-06-22 21:22:30 -0400

    added code for welfare, need to update description

--- main.lua
+++ main.lua
@@ -596,6 +596,52 @@ SMODS.Joker{
         return true
     end,
 }
+SMODS.Joker{
+    key = 'welfare', --joker key
+    loc_txt = { -- local text
+        name = 'Welfare',
+        text = {
+          'Earn {C:money}$#1#{} if',
+          'played hand scores under',
+          '{C:attention}1500{} points'
+        },
+        --[[unlock = {
+            'Be {C:legendary}cool{}',
+        }]]
+    },
+    atlas = 'Jokers', --atlas' key
+    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 4, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 0, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    config = { 
+      extra = {
+        money_cap = 10 --configurable value
+      }
+    },
+    loc_vars = function(self,info_queue,center)
+        return {vars = {center.ability.extra.money_cap}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+    calculate = function(self,card,context)
+        if context.end_of_round and not context.individual and G.GAME.dollars < card.ability.extra.money_cap then
+            return {
+                card = card,
+                dollars = card.ability.extra.money_cap - G.GAME.dollars,
+                message = 'Welfare',
+                colour = G.C.MONEY
+            }
+        end
+    end,
+    in_pool = function(self,wawa,wawa2)
+        --whether or not this card is in the pool, return true if it is, return false if its not
+        return true
+    end,
+}
 ----------------------------------------------
 ------------MOD CODE END----------------------
     
