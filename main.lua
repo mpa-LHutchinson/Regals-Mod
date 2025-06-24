ec43d9e0ddaa6327a5c361591cfef76a2ebdaf25
Liam Hutchinson <liamhutch1000@gmail.com>
2025-06-23 23:34:46 -0400

    added code for euchre

--- main.lua
+++ main.lua
@@ -1045,6 +1045,50 @@ SMODS.Joker{
         end
     end,
 }
+SMODS.Joker{
+    key = 'euchre', --joker key
+    loc_txt = { -- local text
+        name = 'Euchre',
+        text = {
+          'Each {C:attention}Jack{} held in',
+          'hand at the end of',
+          'the round earns {C:money}$#1#{}'
+        },
+        --[[unlock = {
+            'Be {C:legendary}cool{}',
+        }]]
+    },
+    atlas = 'Jokers', --atlas' key
+    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 5, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 1, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    config = { 
+      extra = {
+        money = 2 --configurable value
+      }
+    },
+    loc_vars = function(self,info_queue,center)
+        return {vars = {center.ability.extra.money}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+    calculate = function(self,card,context)
+        if context.cardarea == G.hand and context.individual and context.end_of_round and context.other_card:get_id() == 11 then
+            return {
+                card = card,
+                dollars = card.ability.extra.money
+            }
+        end
+    end,
+    in_pool = function(self,wawa,wawa2)
+        --whether or not this card is in the pool, return true if it is, return false if its not
+        return true
+    end,
+}
 ----------------------------------------------
 ------------MOD CODE END----------------------
     
