c57a9a9ccc71c65095b9c8ec7dbb1329c9124b21
Liam Hutchinson <liamhutch1000@gmail.com>
2025-06-24 21:39:23 -0400

    added code for bubble wrap

--- main.lua
+++ main.lua
@@ -1347,6 +1347,52 @@ SMODS.Joker{
         return false
     end,
 }
+SMODS.Joker{
+    key = 'bubblewrap', --joker key
+    loc_txt = { -- local text
+        name = 'Bubble Wrap',
+        text = {
+          '{C:chips}+#1#{} Chips for',
+          'each {C:attention}consumable{} in your',
+          '{C:attention}consumable{} area',
+        },
+        --[[unlock = {
+            'Be {C:legendary}cool{}',
+        }]]
+    },
+    atlas = 'Jokers', --atlas' key
+    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 4, --cost
+    unlocked = false, --where it is unlocked or not: if true, 
+    discovered = false, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 3, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    config = { 
+      extra = {
+        chip_mod = 40
+      }
+    },
+    loc_vars = function(self,info_queue,center)
+        return {vars = {center.ability.extra.chip_mod}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+    calculate = function(self,card,context)
+        if context.joker_main and G.consumeables.cards[1] then
+            return {
+                card = card,
+                chip_mod = #G.consumeables.cards * card.ability.extra.chip_mod,
+                message = '+' .. #G.consumeables.cards * card.ability.extra.chip_mod,
+                colour = G.C.CHIPS
+            }
+        end
+    end,
+    in_pool = function(self,wawa,wawa2)
+        --whether or not this card is in the pool, return true if it is, return false if its not
+        return true
+    end,
+}
 ----------------------------------------------
 ------------MOD CODE END----------------------
     
