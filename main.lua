d34a1f6476eebd8e3e7139c19491f769b64508eb
Liam Hutchinson <liamhutch1000@gmail.com>
2025-06-25 00:23:55 -0400

    three musketeers code added

--- main.lua
+++ main.lua
@@ -888,7 +888,7 @@ SMODS.Joker{
     blueprint_compat = true, --can it be blueprinted/brainstormed/other
     eternal_compat = true, --can it be eternal
     perishable_compat = true, --can it be perishable
-    pos = {x = 4, y = 1}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    pos = {x = 5, y = 1}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
     config = { 
       extra = {
         Xmult = 1,
@@ -1465,69 +1465,53 @@ SMODS.Joker{
         return false
     end,
 }
+
 SMODS.Joker{
     key = 'threemusketeers', --joker key
     loc_txt = { -- local text
-        name = 'Three musketeers',
+        name = 'Three Musketeers',
         text = {
-          'If played hand contains 3',
-          'scoring {C:attention}7s{}, score',
-          '{C:mult}+#2# mult{} and earn {C:money}$#1#',
-          'at the end of the round'
+          'Each scored {C:attention}3{} will',
+          'earn between {C:money}$#1#{} and {C:money}$#2#{}'
         },
         --[[unlock = {
             'Be {C:legendary}cool{}',
         }]]
     },
     atlas = 'Jokers', --atlas' key
-    rarity = 2, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    rarity = 1, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
     --soul_pos = { x = 0, y = 0 },
-    cost = 5, --cost
-    unlocked = false, --where it is unlocked or not: if true, 
-    discovered = false, --whether or not it starts discovered
+    cost = 6, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
     blueprint_compat = true, --can it be blueprinted/brainstormed/other
     eternal_compat = true, --can it be eternal
     perishable_compat = true, --can it be perishable
     pos = {x = 1, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
     config = { 
       extra = {
-        money = 20,
-        mult = 20,
-        jackpot = false
+        min_money = 1,
+        max_money = 3,
       }
     },
     loc_vars = function(self,info_queue,center)
-        return {vars = {center.ability.extra.money, center.ability.extra.mult, center.ability.extra.jackpot}} --#1# is replaced with card.ability.extra.Xmult
+        return {vars = {center.ability.extra.min_money, center.ability.extra.max_money}} --#1# is replaced with card.ability.extra.Xmult
     end,
     calculate = function(self,card,context)
-        if context.cardarea == G.jokers and context.joker_main then
-            local _sevens = 0
-            for i = 1, #context.scoring_hand do
-                if context.scoring_hand[i]:get_id() == 7 then
-                    _sevens = _sevens + 1
-                end
-            end
-
-            if _sevens >= 3 then
-                card.ability.extra.jackpot = true
-                local eval = function(card) return (card.ability.extra.jackpot == true) end
-                juice_card_until(card, eval, true)
+        if context.individual and context.cardarea == G.play and context.other_card:get_id() == 3 then
+            local earned = math.random(card.ability.extra.min_money, card.ability.extra.max_money)
                 return {
                     card = card,
-                    mult_mod = card.ability.extra.mult,
-                    message = 'Jackpot!',
-                    colour = G.C.MONEY,
+                    dollars = earned,
                 }
-            end
+        end
+    end,
+    in_pool = function(self,wawa,wawa2)
+        --whether or not this card is in the pool, return true if it is, return false if its not
+        return false
+    end,
+}
 
-        end
-    end,
-    calc_dollar_bonus = function(self,card)
-        if card.ability.extra.jackpot then
-            card.ability.extra.jackpot = false
-            return card.ability.extra.money
-        end
-    end,
 ----------------------------------------------
 ------------MOD CODE END----------------------
     
