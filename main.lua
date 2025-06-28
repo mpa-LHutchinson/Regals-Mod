383aaccbf1d6fa4b5e62d3446e5f358cda3817e0
Liam Hutchinson <liamhutch1000@gmail.com>
2025-06-28 03:08:20 -0400

    idk

--- main.lua
+++ main.lua
@@ -1992,8 +1992,8 @@ SMODS.Joker{
     rarity = 2, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
     --soul_pos = { x = 0, y = 0 },
     cost = 7, --cost
-    unlocked = true, --where it is unlocked or not: if true, 
-    discovered = true, --whether or not it starts discovered
+    unlocked = false, --where it is unlocked or not: if true, 
+    discovered = false, --whether or not it starts discovered
     blueprint_compat = true, --can it be blueprinted/brainstormed/other
     eternal_compat = true, --can it be eternal
     perishable_compat = true, --can it be perishable
