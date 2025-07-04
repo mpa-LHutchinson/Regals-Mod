65c14ad79d39c8e1a6ed1af2f5c1c43836d3cc7e
Liam Hutchinson <liamhutch1000@gmail.com>
2025-07-04 00:34:30 -0400

    bubble wrap coordinates

--- main.lua
+++ main.lua
@@ -1404,7 +1404,7 @@ SMODS.Joker{
     blueprint_compat = true, --can it be blueprinted/brainstormed/other
     eternal_compat = true, --can it be eternal
     perishable_compat = true, --can it be perishable
-    pos = {x = 3, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    pos = {x = 2, y = 2}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
     config = { 
       extra = {
         chip_mod = 45
