b5bbf2c9dc23a6bf34f756f179c9d52a88603d72
Liam Hutchinson <liamhutch1000@gmail.com>
2025-07-10 17:08:28 -0400

    added john polychrome code

--- main.lua
+++ main.lua
@@ -2765,6 +2765,55 @@ SMODS.Joker{
         return false
     end,
 }
+SMODS.Joker{
+    key = 'johnpolychrome', --joker key
+    loc_txt = { -- local text
+        name = 'John Polychrome',
+        text = {
+          'Converts all scored cards',
+          'from {C:attention}first hand{} each',
+          'round into {C:attention}Stone cards{}'
+        },
+        --[[unlock = {
+            'Be {C:legendary}cool{}',
+        }]]
+    },
+    atlas = 'Jokers', --atlas' key
+    rarity = 4, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 20, --cost
+    unlocked = false, --where it is unlocked or not: if true, 
+    discovered = false, --whether or not it starts discovered
+    blueprint_compat = false, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 2, y = 1}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    config = { 
+      extra = {
+      }
+    },
+    loc_vars = function(self,info_queue,center)
+        return {vars = {}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+    calculate = function(self, card, context)
+        if context.before and context.cardarea == G.jokers then
+            for k, v in ipairs(context.scoring_hand) do
+                v:set_edition('e_polychrome', true)
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        v:juice_up()
+                        return true
+                    end
+                })) 
+            end
+        end
+    end,
+
+    in_pool = function(self,wawa,wawa2)
+        --whether or not this card is in the pool, return true if it is, return false if its not
+        return false
+    end,
+}
 ----------------------------------------------
 ------------MOD CODE END----------------------
     
