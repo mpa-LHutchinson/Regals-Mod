85ded0c64c7e7ca22f9fa45f98ff58787375d9be
Liam Hutchinson <liamhutch1000@gmail.com>
2025-11-12 17:36:50 -0500

    city of tears nerf

--- main.lua
+++ main.lua
@@ -3168,7 +3168,7 @@ SMODS.Joker{
     pos = {x = 4, y = 5}, 
     config = { 
       extra = {
-        tear_chips = 20
+        tear_chips = 15
       }
     },
     loc_vars = function(self,info_queue,center)
